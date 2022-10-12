f0e116937b31f683786c8e033feb9c3b021a5db2
otreci4sgelt0nas <96852897+otreci4sgelt0nas@users.noreply.github.com>
2022-10-12 08:20:24 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="background">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+
+</body>
